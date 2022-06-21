4bd04862e29ce69097065eae9e8ba0c2a3f47d71
BBergsJ <goldlike888@gmail.com>
2022-06-21 23:08:04 +0300

    Cinema

---
+++ src/main/java/ru/job4j/arrays/Cinema.java
@@ -0,0 +1,105 @@
+package ru.job4j.arrays;
+
+import java.util.Objects;
+
+public class Cinema {
+    public static Place checkEmptyPlace(Place[][] places) {
+//        for (int i = 0; i < places.length - 1; i++) {
+//            for (int j = 0; j < places[i].length; j++) {
+//                if (places[i][j] == null
+//                        && places[i + 1][j] == null
+//                        && places[i][j + 1] == null) {
+//                    return new Place(i, j);
+//                }
+//            }
+//        }
+//        return null;
+
+//        for (int i = 0; i < places.length; i++) {
+//            for (int j = 0; j < places[i].length; j++) {
+//                Place place = places[i][j];
+//                Place one = null;
+//                Place two = null;
+//                Place three = null;
+//                Place four = null;
+//                if (i - 1 >= 0) {
+//                    one = places[i - 1][j];
+//                }
+//                if (i + 1 != places.length) {
+//                    two = places[i + 1][j];
+//                }
+//                if (j + 1 != places[i].length) {
+//                    three = places[i][j + 1];
+//                }
+//                if (j - 1 >= 0) {
+//                    four = places[i][j - 1];
+//                }
+//                if (place == null) {
+//                    if (one == null && two == null && three == null && four == null) {
+//                        return new Place(i, j);
+//                    }
+//                }
+//            }
+//        }
+//        return null;
+
+        for (int i = 0; i < places.length; i++) {
+            for (int j = 0; j < places[i].length; j++) {
+                if (places[i][j] == null) {
+                    Place left = null;
+                    Place right = null;
+                    Place up = null;
+                    Place down = null;
+                    if (i - 1 >= 0) {
+                        up = places[i - 1][j];
+                    }
+                    if (i + 1 != places.length) {
+                        down = places[i + 1][j];
+                    }
+                    if (j - 1 >= 0) {
+                        left = places[i][j - 1];
+                    }
+                    if (j + 1 != places[i].length) {
+                        right = places[i][j + 1];
+                    }
+                    if (left == null
+                            && right == null
+                            && up == null
+                            && down == null) {
+                        return new Place(i, j);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static class Place {
+        private int row;
+
+        private int cell;
+
+        public Place(int row, int cell) {
+            this.row = row;
+            this.cell = cell;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Place place = (Place) o;
+            return row == place.row
+                    && cell == place.cell;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, cell);
+        }
+    }
+}
