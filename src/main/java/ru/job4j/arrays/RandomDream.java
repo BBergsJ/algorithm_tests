fb7944581d23da20872401a5fcb1e3f40edc79d0
BBergsJ <goldlike888@gmail.com>
2022-06-24 00:02:44 +0300

    Поле чудес

---
+++ src/main/java/ru/job4j/arrays/RandomDream.java
@@ -0,0 +1,15 @@
+package ru.job4j.arrays;
+
+public class RandomDream {
+    public static String random(String[] prizes, int num) {
+        return num % prizes.length == 1
+                ? prizes[0]
+                : num % prizes.length == 2
+                ? prizes[1]
+                : num % prizes.length == 3
+                ? prizes[2]
+                : num % prizes.length == 0
+                ? prizes[3]
+                : "null";
+    }
+}
