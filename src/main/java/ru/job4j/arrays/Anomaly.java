6aa5092f8de30414c296af8fd40977802ddd587d
BBergsJ <goldlike888@gmail.com>
2022-07-01 23:23:59 +0300

    Аномалии

---
+++ src/main/java/ru/job4j/arrays/Anomaly.java
@@ -0,0 +1,12 @@
+package ru.job4j.arrays;
+
+import java.util.Arrays;
+
+public class Anomaly {
+    public static int[][] found(int[] data, int up, int down) {
+        int[][] result = new int[data.length][];
+        int count = 0;
+
+        return Arrays.copyOf(result, count);
+    }
+}
