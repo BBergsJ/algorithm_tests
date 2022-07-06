1462bdbaf34a8016a7f77d86efb6fe866183e237
BBergsJ <goldlike888@gmail.com>
2022-07-07 01:23:36 +0300

    5.9. Аномалии

--- src/main/java/ru/job4j/arrays/Anomaly.java
+++ src/main/java/ru/job4j/arrays/Anomaly.java
@@ -6,7 +6,19 @@ public class Anomaly {
     public static int[][] found(int[] data, int up, int down) {
         int[][] result = new int[data.length][];
         int count = 0;
-
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] >= up || data[i] <= down) {
+                if (result[count] == null) {
+                    result[count] = new int[]{i, i};
+                } else {
+                    result[count][1] = i;
+                    count++;
+                }
+            }
+        }
+        if (result[0] != null && result[1] == null) {
+            count = 1;
+        }
         return Arrays.copyOf(result, count);
     }
 }
