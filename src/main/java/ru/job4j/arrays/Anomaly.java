723a8c343e91752a8be26cb8369fe2be084fb38f
BBergsJ <goldlike888@gmail.com>
2022-07-07 17:13:20 +0300

    5.9. Аномалии correction

--- src/main/java/ru/job4j/arrays/Anomaly.java
+++ src/main/java/ru/job4j/arrays/Anomaly.java
@@ -7,18 +7,50 @@ public class Anomaly {
         int[][] result = new int[data.length][];
         int count = 0;
         for (int i = 0; i < data.length; i++) {
-            if (data[i] >= up || data[i] <= down) {
-                if (result[count] == null) {
-                    result[count] = new int[]{i, i};
-                } else {
+            if (data[i] <= down || data[i] >= up) {
+                result[count] = new int[]{i, i};
+                while (i < data.length && (data[i] <= down || data[i] >= up)) {
                     result[count][1] = i;
-                    count++;
+                    i++;
                 }
+                count++;
             }
         }
-        if (result[0] != null && result[1] == null) {
-            count = 1;
-        }
         return Arrays.copyOf(result, count);
+
+//        int[][] result = new int[data.length][];
+//        int count = 0;
+//        for (int i = 0; i < data.length; i++) {
+//            if (data[i] >= up || data[i] <= down) {
+//                if (result[count] == null) {
+//                    result[count] = new int[]{i, i};
+//                } else {
+//                    result[count][1] = i;
+//                    count++;
+//                }
+//            }
+//        }
+//        if (result[0] != null && result[1] == null) {
+//            count = 1;
+//        }
+//        return Arrays.copyOf(result, count);
+
+//        int[][] result = new int[data.length][];
+//        int count = 0;
+//        for (int i = 0; i < data.length; i++) {
+//            if (data[i] >= up || data[i] <= down) {
+//                int[] buffer = new int[2];
+//                buffer[0] = i;
+//                while (data[i] >= up || data[i] <= down) {
+//                    buffer[1] = i;
+//                    i++;
+//                    if (data.length - 1 < i) {
+//                        break;
+//                    }
+//                }
+//                result[count++] = buffer;
+//            }
+//        }
+//        return Arrays.copyOf(result, count);
     }
 }
