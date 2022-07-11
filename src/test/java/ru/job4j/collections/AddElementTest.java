9daabaea926b4d62ced293340e4d28d0140dab27
BBergsJ <goldlike888@gmail.com>
2022-07-11 16:29:23 +0300

    0.2. Размер списка и добавление элементов.

---
+++ src/test/java/ru/job4j/collections/AddElementTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.collections;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddElementTest {
+    @Test
+    public void addNewElement() {
+        List<String> list = new ArrayList<>();
+        list.add("first");
+        list.add("second");
+        list.add("third");
+        boolean result = AddElement.addNewElement(list, "fourth");
+        assertThat(result, is(true));
+    }
+}
