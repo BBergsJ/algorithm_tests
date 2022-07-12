3c4dd9a8b5e1da31d3029f8cc42013ba163ea1e6
BBergsJ <goldlike888@gmail.com>
2022-07-13 00:31:54 +0300

    0.5. Заполнение списка фабричным методом

--- src/main/java/ru/job4j/collections/UniqueElement.java
+++ src/main/java/ru/job4j/collections/UniqueElement.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 public class UniqueElement {
     public static boolean checkList(List<String> list, String str) {
-        if (list.indexOf(str) == list.lastIndexOf(str)
-                && list.indexOf(str) != -1) {
-            return true;
-        }
-        return false;
+        return list.indexOf(str) == list.lastIndexOf(str)
+                && list.indexOf(str) != -1;
     }
 }
---
+++ src/test/java/ru/job4j/collections/FactoryTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.collections;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactoryTest {
+    @Test
+    public void main() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Factory.main(null);
+        String ln = System.lineSeparator();
+        String expected = "first" + ln + "second"
+                + ln + "third"
+                + ln + "fourth"
+                + ln + "fifth" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+}
