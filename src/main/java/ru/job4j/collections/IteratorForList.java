a2d60c3c3aa277958b05236ad886c28fc68c63b1
BBergsJ <goldlike888@gmail.com>
2022-07-13 17:13:23 +0300

    1.3. ListIterator для списка.

--- src/main/java/ru/job4j/collections/IteratorForList.java
+++ src/main/java/ru/job4j/collections/IteratorForList.java
@@ -8,8 +8,8 @@ public class IteratorForList {
         List<String> list = List.of("one", "two", "three", "four", "five");
         Iterator<String> iterator = list.iterator();
         while (iterator.hasNext()) {
-            System.out.println("Next element has - " + "true" + ", this element is " + iterator.next());
+            System.out.println("Next element has - " + iterator.hasNext() + ", this element is " + iterator.next());
         }
-        System.out.println("Next element has - " + "false");
+        System.out.println("Next element has - " + iterator.hasNext());
     }
 }
