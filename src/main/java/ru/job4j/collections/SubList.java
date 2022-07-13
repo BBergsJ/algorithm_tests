e2153a89ce327357cc27214696ebb8423206f68f
BBergsJ <goldlike888@gmail.com>
2022-07-13 16:19:40 +0300

    0.9. Получить часть списка с определенными индексами

---
+++ src/main/java/ru/job4j/collections/SubList.java
@@ -0,0 +1,12 @@
+package ru.job4j.collections;
+
+import java.util.List;
+
+public class SubList {
+    public static List<String> getElementsBetweenIndexes(List<String> list, String el) {
+        if (list.indexOf(el) == list.lastIndexOf(el) || !list.contains(el)) {
+            return List.of();
+        }
+        return list.subList(list.indexOf(el), list.lastIndexOf(el));
+    }
+}
