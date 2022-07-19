0d7e6dab137688deab0ccad432489a3ab15425e2
BBergsJ <goldlike888@gmail.com>
2022-07-19 23:17:57 +0300

    1.2. Добавление элемента в отображение без проверки.

---
+++ src/test/java/ru/job4j/map/PutIfAbsentTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.map;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PutIfAbsentTest {
+    @Test
+    public void addNewElement() {
+        Map<Integer, PutIfAbsent.User> map = new HashMap<>(
+                Map.of(
+                        1, new PutIfAbsent.User(1, "Name1"),
+                        2, new PutIfAbsent.User(2, "Name2")
+                )
+        );
+        PutIfAbsent put = new PutIfAbsent(map);
+        PutIfAbsent.User user = new PutIfAbsent.User(1, "Name3");
+        boolean rsl = put.addNewElement(user);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void addNewElementTrue() {
+        Map<Integer, PutIfAbsent.User> map = new HashMap<>(
+                Map.of(
+                        1, new PutIfAbsent.User(1, "Name1"),
+                        2, new PutIfAbsent.User(2, "Name2")
+                )
+        );
+        PutIfAbsent put = new PutIfAbsent(map);
+        PutIfAbsent.User user = new PutIfAbsent.User(3, "Name3");
+        boolean rsl = put.addNewElement(user);
+        assertThat(rsl, is(true));
+    }
+}
