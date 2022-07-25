4c794775459853d8c065afca7f398a276e0852a2
BBergsJ <goldlike888@gmail.com>
2022-07-25 22:06:20 +0300

    1.5. Изменение уже существующей записи в отображении 2

--- src/main/java/ru/job4j/map/ComputeIfPresent.java
+++ src/main/java/ru/job4j/map/ComputeIfPresent.java
@@ -8,8 +8,11 @@ import java.util.Map;
 public class ComputeIfPresent {
     public static Map<Integer, String> collectData(
             Map<Integer, String> name, Map<Integer, String> surname) {
-        for (Map.Entry<Integer, String> entry : surname.entrySet()) {
-            name.computeIfPresent(entry.getKey(), (key, value) -> value + " " + surname.get(key));
+//        for (Map.Entry<Integer, String> entry : surname.entrySet()) {
+//            name.computeIfPresent(entry.getKey(), (key, value) -> value + " " + surname.get(key));
+//        }
+        for (Integer integer : surname.keySet()) {
+            name.computeIfPresent(integer, (k, v) -> v + " " + surname.get(integer));
         }
         return name;
     }
