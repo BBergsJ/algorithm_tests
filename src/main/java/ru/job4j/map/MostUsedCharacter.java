95e2cda12e46731775ee9ed4301ca840169ae3b8
BBergsJ <goldlike888@gmail.com>
2022-07-28 23:37:04 +0300

    1.7. Найти символ который используется в строке наибольшее количество раз

--- src/main/java/ru/job4j/map/MostUsedCharacter.java
+++ src/main/java/ru/job4j/map/MostUsedCharacter.java
@@ -6,17 +6,17 @@ import java.util.TreeMap;
 public class MostUsedCharacter {
     public static char getMaxCount(String str) {
         Map<Character, Integer> map = new TreeMap<>();
-        String tmp =  str.toLowerCase().replace(" ", "");
+        String tmp =  str.toLowerCase().replaceAll("\\s+", "");
         for (Character c : tmp.toCharArray()) {
             map.computeIfPresent(c, (k, v) -> v + 1);
             map.putIfAbsent(c, 1);
         }
         char rsl = ' ';
         int i = 0;
-        for (Map.Entry<Character, Integer> m : map.entrySet()) {
-            if (m.getValue() > i) {
-                i = m.getValue();
-                rsl = m.getKey();
+        for (Character c : map.keySet()) {
+            if (map.get(c) > i) {
+                i = map.get(c);
+                rsl = c;
             }
         }
         return rsl;
