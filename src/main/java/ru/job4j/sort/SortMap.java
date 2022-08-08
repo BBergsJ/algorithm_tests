32576910766644e1014d37a5b1e7de3f75c1e4c8
BBergsJ <goldlike888@gmail.com>
2022-08-08 16:54:31 +0300

    2.7. Способы сортировки. Сортировка Map.

---
+++ src/main/java/ru/job4j/sort/SortMap.java
@@ -0,0 +1,10 @@
+package ru.job4j.sort;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SortMap {
+    public static Map<Integer, String> sort(Map<Integer, String> map) {
+        return new TreeMap<>(map);
+    }
+}
