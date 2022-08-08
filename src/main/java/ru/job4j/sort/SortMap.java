ac903eb8e4bff5ae18cab2eaaad62e3f5d4b5c9a
BBergsJ <goldlike888@gmail.com>
2022-08-08 16:58:10 +0300

    2.7. Способы сортировки. Сортировка Map.

--- src/main/java/ru/job4j/sort/SortMap.java
+++ src/main/java/ru/job4j/sort/SortMap.java
@@ -1,10 +1,13 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class SortMap {
     public static Map<Integer, String> sort(Map<Integer, String> map) {
-        return new TreeMap<>(map);
+        Map<Integer, String> rsl = new TreeMap<>(Comparator.reverseOrder());
+        rsl.putAll(map);
+        return rsl;
     }
 }
