2495ecf72c7839693baa2d8a3ea9c01fd0eae893
BBergsJ <goldlike888@gmail.com>
2022-08-10 16:44:43 +0300

    5.3. Optional. Метод ifPresent()

---
+++ src/main/java/ru/job4j/lambda/OptionalIfPresent.java
@@ -0,0 +1,14 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class OptionalIfPresent {
+    public static void ifPresent(int[] data) {
+        max(data).ifPresent(x -> System.out.println("Max: " + x));
+    }
+
+    private static Optional<Integer> max(int[] data) {
+        return data.length == 0 ? Optional.empty() : Optional.of(Arrays.stream(data).max().getAsInt());
+    }
+}
