73ae11503f35e18a980ac858470a26a25d150b1e
BBergsJ <goldlike888@gmail.com>
2022-08-14 00:50:34 +0300

    1.2. Создание стрима из последовательности. Метод Stream.of()

---
+++ src/test/java/ru/job4j/stream/StreamOfMethodTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StreamOfMethodTest {
+    @Test
+    public void whenUserStreamMethod() {
+        assertEquals("abc",
+                StreamOfMethod.createStream().map(Object::toString).collect(Collectors.joining()));
+    }
+}
