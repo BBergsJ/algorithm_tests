53ba657cd35b3457e5b37ef4d4d09f68f0a7b486
BBergsJ <goldlike888@gmail.com>
2022-08-18 00:09:37 +0300

    1.7. Создание стрима примитивов. Методы range() и rangeClosed()

---
+++ src/test/java/ru/job4j/stream/RangeMethodTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangeMethodTest {
+    @Test
+    public void test() {
+        List<Integer> result = RangeMethod.createStream(1, 3).boxed().collect(Collectors.toList());
+        List<Integer> expect = List.of(1, 2, 3);
+        assertEquals(expect, result);
+    }
+}
