9c1846fa427bf3116f93b3fe10e5442decae45b0
BBergsJ <goldlike888@gmail.com>
2022-08-24 17:15:36 +0300

    4.4. Терминальные операции. Метод noneMatch()

---
+++ src/test/java/ru/job4j/stream/NoneMatchMethodTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoneMatchMethodTest {
+    @Test
+    public void test() {
+        assertTrue(NoneMatchMethod.check(List.of("1", "2", "3")));
+        assertFalse(NoneMatchMethod.check(List.of("1", "", "3")));
+    }
+}
