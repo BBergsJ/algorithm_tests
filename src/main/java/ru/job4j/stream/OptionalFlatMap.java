22335da8780c0e9fc5d63565b7eca1e4763795b1
BBergsJ <goldlike888@gmail.com>
2022-08-26 22:47:57 +0300

    9.6. Optional и Stream. Метод flatMap()

---
+++ src/main/java/ru/job4j/stream/OptionalFlatMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OptionalFlatMap {
+    public static Optional<Integer> flatMap(List<String> strings) {
+        return strings.stream()
+                .filter(s -> s.endsWith(".java"))
+                .findFirst()
+                .flatMap(f -> Optional.of(f.length()));
+    }
+}
